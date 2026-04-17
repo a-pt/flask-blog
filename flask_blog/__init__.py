bbb464d4cad778627497ed9afb07bd0ffe0beb3b
Athira PT <athirapt998@gmail.com>
2026-04-17 18:41:58 +0530

    add config variables to env file

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,18 +1,13 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_blog.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5214a2e37461069d1df2a668234e1374'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 mail = Mail(app)
