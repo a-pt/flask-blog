7e69daad8880b2ea0eaafd7db1cb0f2a23f45eae
Athira PT <athirapt998@gmail.com>
2026-04-19 12:20:04 +0530

    routing changes

--- flask_blog/main/routes.py
+++ flask_blog/main/routes.py
@@ -4,13 +4,13 @@ from flask_login import current_user, login_required
 
 main = Blueprint('main', __name__)
 
-@main.route("/")
 @main.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5, page=page)
     return render_template("home.html",posts=posts)
 
+@main.route("/")
 @main.route("/about")
 def about():
     return render_template("about.html",title="About")
