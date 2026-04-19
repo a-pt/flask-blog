ae9275aacb76c9759ab25a33743bc314c4f7f689
Athira PT <athirapt998@gmail.com>
2026-04-19 12:56:18 +0530

    Switch to external Postgres (Supabase) for persistent storage

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -4,8 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.getenv('POSTGRES_URL')
+    _url = os.getenv('DATABASE_URL')
+    if _url:
+        SQLALCHEMY_DATABASE_URI = _url.replace('postgres://', 'postgresql://', 1)
+    else:
+        SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or 'sqlite:///site.db'
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
